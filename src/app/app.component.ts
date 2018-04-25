389565e7128bf6ad9332d244abd35fa6168fddf0
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-25 12:26:34 +0530

    Easy sudoku can be resolved easily

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import * as _ from 'lodash';
 import { Entry } from './Entry';
 import { SquareMatrix } from './SquareMatrix';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
 	selector: 'app-root',
@@ -12,6 +13,8 @@ export class AppComponent {
 	title = 'app';
 	data = [];
 	counter = 0;
+	subject = new Subject<any>();
+
 	constructor() {
 		for (let i = 0;i < 9;i++) {
 			this.data[i] = Array(9);
@@ -19,28 +22,41 @@ export class AppComponent {
 				this.data[i][j] = new Entry(i, j);
 			}
 		}
+		this.subject.subscribe(({ row, column, value }) => {
+			this.data[row][column].value = this.data[row][column].possibility[0];
+			this.resetPossiblity(row, column);
+			if (this.counter < 81) {
+				setTimeout(() => {
+					this.startSolving();
+				}, 1000);
+			}
+		}, (err) => console.log(err));
 	}
 	startSolving = () => {
-		this.counter++;
 		for (let i = 0;i < 9;i++) {
 			for (let j = 0;j < 9;j++) {
-				if(this.data[i][j].possibility.length === 1) {
-					this.data[i][j].value = this.data[i][j].possibility[0];
-					this.resetPossiblity(i, j, this.data[i][j].squareNo, this.data[i][j].value);
+				if (this.data[i][j].possibility.length === 1) {
+					this.subject.next({ row: i, column: j, value: this.data[i][j].possibility[0] });
+
 					// console.log("Run Count => ", this.counter);
 					// console.log("Resolved Entry => Row(",i,"), Column(", j,") with value=", this.data[i][j].possibility[0]);
 					return;
 				}
 			}
 		}
+		alert("Sorry, this sudoku lies under difficult category, will be resolved by next version.")
 	}
-	resetPossiblity = (row, column, squareNum, value) => {
+	resetPossiblity = (row, column) => {
+		const squareNum = this.data[row][column].squareNo;
+		var value;
 		var possibility = [], index = -1;
-		if(value != "") {
-			value = +value;
+
+		if (this.data[row][column].value != "") {
+			this.counter++;
+			value = +this.data[row][column].value;
 			_.forEach(this.data[row], (entry) => {
 				index = entry.possibility.indexOf(value);
-				if(index > -1) {
+				if (index > -1) {
 					entry.possibility.splice(index, 1);
 				}
 			});
@@ -62,7 +78,9 @@ export class AppComponent {
 			}
 			this.data[row][column].possibility.splice(0, 9);
 			//console.log(this.data)
+		} else {
+
 		}
-		
+
 	}
 }
